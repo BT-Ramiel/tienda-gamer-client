cb72a7702ec0cc24f6ecb8220bb418fe0db050c7
Xenofixion <davila.bojorquez@gmail.com>
2021-07-02 16:34:44 -0600

    :working: - added validation to register form

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,25 +1,53 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export default function RegisterForm({ showLoginForm = null }) {
+	const { handleSubmit, handleChange, errors } = useFormik({
+		initialValues: initialValues(),
+		validationSchema: Yup.object(validationSchema()),
+		onSubmit: formData => {
+			console.log(formData)
+		},
+	})
+
 	return (
-		<Form className="RegisterForm">
-			<Form.Input name="name" type="text" placeholder="Nombre" />
-			<Form.Input name="lastname" type="text" placeholder="Apellidos" />
+		<Form className="RegisterForm" onSubmit={handleSubmit}>
+			<Form.Input
+				name="name"
+				type="text"
+				placeholder="Nombre"
+				onChange={handleChange}
+				error={errors.name}
+			/>
+			<Form.Input
+				name="lastname"
+				type="text"
+				placeholder="Apellidos"
+				onChange={handleChange}
+				error={errors.lastname}
+			/>
 			<Form.Input
 				name="username"
 				type="text"
 				placeholder="Nombre de usuario"
+				onChange={handleChange}
+				error={errors.username}
 			/>
 			<Form.Input
 				name="email"
 				type="text"
 				placeholder="Correo electronico"
+				onChange={handleChange}
+				error={errors.email}
 			/>
 			<Form.Input
 				name="password"
 				type="password"
 				placeholder="Contrasena"
+				onChange={handleChange}
+				error={errors.password}
 			/>
 			<div className="buttons-container">
 				<Button type="button" basic>
@@ -31,4 +59,18 @@ export default function RegisterForm({ showLoginForm = null }) {
 			</div>
 		</Form>
 	)
+}
+
+function initialValues() {
+	return { name: '', lastname: '', username: '', email: '', password: '' }
+}
+
+function validationSchema() {
+	return {
+		name: Yup.string().required(true),
+		lastname: Yup.string().required(true),
+		username: Yup.string().required(true),
+		email: Yup.string().email(true).required(true),
+		password: Yup.string().required(true),
+	}
 }
