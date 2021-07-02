03a10f7ab1d3464726b0a7bab92a915ae5552731
Xenofixion <davila.bojorquez@gmail.com>
2021-07-02 15:22:44 -0600

    :lipstick: - added sass global style to inputs and form submit buttons

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'semantic-ui-react'
 
 export default function RegisterForm({ showLoginForm = null }) {
 	return (
-		<div>
-			<h1>Formulario de registro</h1>
-			<button onClick={showLoginForm}>Ir al login</button>
-		</div>
+		<Form className="RegisterForm">
+			<Form.Input name="name" type="text" placeholder="Nombre" />
+			<Form.Input name="lastname" type="text" placeholder="Apellidos" />
+			<Form.Input
+				name="username"
+				type="text"
+				placeholder="Nombre de usuario"
+			/>
+			<Form.Input
+				name="email"
+				type="text"
+				placeholder="Correo electronico"
+			/>
+			<Form.Input
+				name="password"
+				type="password"
+				placeholder="Contrasena"
+			/>
+			<div className="buttons-container">
+				<Button type="button" basic>
+					Iniciar sesion
+				</Button>
+				<Button type="submit" className="submit-button">
+					Registrar
+				</Button>
+			</div>
+		</Form>
 	)
 }
